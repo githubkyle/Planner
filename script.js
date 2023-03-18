8ce9aac27607ae8f46ab9c45448a671bda0cf8c1
kshwi <kshwiss@gmail.com>
2023-03-18 12:28:10 -0500

    met with tutor and worked out final kinks

--- script.js
+++ script.js
@@ -19,19 +19,19 @@ $(function() {
   // var 5pm = $('input[id="hour-17"]').val();
   // var 6pm = $('input[id="hour-18"]').val();
 
-  var Seven = document.getElementById("hour-7");
-  var Eight = document.getElementById("hour-8");
-  var Nine = document.getElementById("hour-9");
-  var Ten = document.getElementById("hour-10");
-  var Eleven = document.getElementById("hour-11");
-  var Twelve = document.getElementById("hour-12");
-  var Thirteen = document.getElementById("hour-13");
-  var Fourteen = document.getElementById("hour-14");
-  var Fifteen = document.getElementById("hour-15");
-  var Sixteen = document.getElementById("hour-16");
-  var Seventeen = document.getElementById("hour-17");
-  var Eightteen = document.getElementById("hour-18");
-  var AllTimes = document.getElementsByClassName("time-block");
+  // var Seven = document.getElementById("hour-7");
+  // var Eight = document.getElementById("hour-8");
+  // var Nine = document.getElementById("hour-9");
+  // var Ten = document.getElementById("hour-10");
+  // var Eleven = document.getElementById("hour-11");
+  // var Twelve = document.getElementById("hour-12");
+  // var Thirteen = document.getElementById("hour-13");
+  // var Fourteen = document.getElementById("hour-14");
+  // var Fifteen = document.getElementById("hour-15");
+  // var Sixteen = document.getElementById("hour-16");
+  // var Seventeen = document.getElementById("hour-17");
+  // var Eightteen = document.getElementById("hour-18");
+  // var AllTimes = document.getElementsByClassName("time-block");
 
   Hourblocks.each(function() {
     console.log(this);
@@ -45,17 +45,17 @@ $(function() {
   });
 
   $("#hour-7 textarea").val(localStorage.getItem("hour-7"));
-  $("#hour-8").text = localStorage.getItem("hour-8");
-  $("#hour-9").text = localStorage.getItem("hour-9");
+  $("#hour-8 textarea").val(localStorage.getItem("hour-8"));
+  $("#hour-9 textarea").val(localStorage.getItem("hour-9"));
   $("#hour-10 textarea").val(localStorage.getItem("hour-10"));
-  $("#hour-11").text = localStorage.getItem("hour-11");
-  $("#hour-12").text = localStorage.getItem("hour-12");
-  $("#hour-13").text = localStorage.getItem("hour-13");
-  $("#hour-14").text = localStorage.getItem("hour-14");
-  $("#hour-15").text = localStorage.getItem("hour-15");
-  $("#hour-16").text = localStorage.getItem("hour-16");
-  $("#hour-17").text = localStorage.getItem("hour-17");
-  $("#hour-18").text = localStorage.getItem("hour-18");
+  $("#hour-11 textarea").val(localStorage.getItem("hour-11"));
+  $("#hour-12 textarea").val(localStorage.getItem("hour-12"));
+  $("#hour-13 textarea").val(localStorage.getItem("hour-13"));
+  $("#hour-14 textarea").val(localStorage.getItem("hour-14"));
+  $("#hour-15 textarea").val(localStorage.getItem("hour-15"));
+  $("#hour-16 textarea").val(localStorage.getItem("hour-16"));
+  $("#hour-17 textarea").val(localStorage.getItem("hour-17"));
+  $("#hour-18 textarea").val(localStorage.getItem("hour-18"));
 
   function Saving() {
     localStorage.setItem(
